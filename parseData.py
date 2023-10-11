ba0515d682aabc6305b4a8224f1eda079ca413e9
Archit Satish Joshi <111919974+JoshiArchit@users.noreply.github.com>
2023-10-11 14:21:09 -0400

    Add files via upload

--- parseData.py
+++ parseData.py
@@ -6,6 +6,8 @@ Language : python3
 """
 import os
 import pandas as pd
+import dpkt
+import socket
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -19,8 +21,26 @@ def scaleData(data):
     return data
 
 
+def create_pcap(data, pcap_file='pcap_data'):
+    with open(pcap_file, 'wb') as f:
+        pcap_writer = dpkt.pcap.Writer(f)
+        for _, row in data.iterrows():
+            eth = dpkt.ethernet.Ethernet()
+
+            eth.data = dpkt.ip.IP()
+            eth.data.data = dpkt.tcp.TCP()
+
+            eth.data.src = socket.inet_aton('127.0.0.1')
+            eth.data.dst = socket.inet_aton('127.0.0.1')
+            eth.data.data.sport = 12345
+            eth.data.data.dport = 80
+            eth.data.data.data = b''
+
+            pcap_writer.writepkt(eth)
+
+
 def main():
-    filepath = os.getcwd() + '//data//dummy'
+    filepath = os.getcwd() + '//data//dummy_data'
     columns = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes',
                'dst_bytes', 'land', 'wrong_fragment', 'urgent', 'hot',
                'num_failed_logins', 'logged_in', 'num_compromised',
@@ -34,11 +54,10 @@ def main():
                'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate',
                'dst_host_serror_rate', 'dst_host_srv_serror_rate',
                'dst_host_rerror_rate', 'dst_host_srv_rerror_rate', 'label']
-    print(len(columns))
+
     dataframe = pd.read_csv(filepath, names=columns)
-    print(dataframe)
     dataframe = scaleData(dataframe)
-    print(dataframe)
+    create_pcap(dataframe)
 
 
 if __name__ == "__main__":
